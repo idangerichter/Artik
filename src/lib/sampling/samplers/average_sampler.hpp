bf6a5e31099151b8ea5c4d8493d248d3d4706e17
Yoav Sternberg <yoav.sternberg@gmail.com>
2019-08-01 20:58:10 +0300

    :bug: Get it working again

--- src/lib/sampling/samplers/average_sampler.hpp
+++ src/lib/sampling/samplers/average_sampler.hpp
@@ -14,7 +14,8 @@ private:
   const std::unique_ptr<Sampler> sampler_;
   const size_t between_rounds_delay_;
   const size_t count_;
-  
+  const size_t data_size_;
+
   // Due to performance issues, we allocate the vector once.
   std::vector<Measurement> temp_measurements_;
   std::vector<Measurement> temp_aggregating_measurements_;
