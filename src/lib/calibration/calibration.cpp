c1308808fe96da0f42ccf1d4d17a50b9c73e0682
Idan Gerichter <idangerichter@gmail.com>
2019-08-11 11:29:54 +0300

    :bug: changed calibration

--- src/lib/calibration/calibration.cpp
+++ src/lib/calibration/calibration.cpp
@@ -3,7 +3,7 @@
 #include "simple_score_provider.hpp"
 #include <algorithm>
 #include <functional>
-
+#include <iostream>
 constexpr double THRESHOLD_FLUSHED_MULTIPLIER = 0.9;
 
 namespace Calibration
@@ -11,23 +11,21 @@ namespace Calibration
 std::unique_ptr<ScoreProvider> Calibrate(std::vector<Measurement>& measurements_after_flush,
                                          AttackType type)
 {
-  std::function<bool(int, int)> comparator = std::less<>();
+  std::function<bool(int, int)> comparator = std::greater<>();
 
   if (type != AttackType::FlushReload)
   {
-    comparator = std::greater<>();
+    comparator = std::less<>();
   }
 
   auto index = static_cast<size_t>(measurements_after_flush.size() * THRESHOLD_FLUSHED_MULTIPLIER);
 
-  auto typeCoerced = type == AttackType ::FlushReload;
   std::nth_element(measurements_after_flush.begin(),
                    measurements_after_flush.begin() + index,
                    measurements_after_flush.end(),
-                   [typeCoerced](Measurement& lhs, Measurement& rhs) -> bool {
-                     return (lhs.time < rhs.time) ^ typeCoerced;
+                   [comparator](Measurement& lhs, Measurement& rhs) -> bool {
+                     return comparator(lhs.time, rhs.time);
                    });
-
 
   auto threshold = measurements_after_flush[index].time;
 
